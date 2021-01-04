e38b9567def6b7b1bf35ea87eca1c3dc902fca17
leo <hojin421@gmail.com>
2021-01-04 23:54:23 +1100

    add proper modem off with the hello_world low power code. add new app example: udp_lp_appCoreOff_test: put app core off after PSM (mode switch to off as well).

--- app/nrf91/blinky_lp/src/main.c
+++ app/nrf91/blinky_lp/src/main.c
@@ -9,31 +9,30 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 
-
 #include <power/power.h>
 #include <hal/nrf_regulators.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   3000
+#define SLEEP_TIME_MS 3000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#define LED0 DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS DT_GPIO_FLAGS(LED0_NODE, gpios)
 #else
 /* A build error here means your board isn't set up to blink an LED. */
 #error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
+#define LED0 ""
+#define PIN 0
+#define FLAGS 0
 #endif
-void disable_uart()  // disable UART to measure current.
+void disable_uart() // disable UART to measure current.
 {
-      NRF_UARTE0->ENABLE = 0;
-      NRF_UARTE1->ENABLE = 0;
+	NRF_UARTE0->ENABLE = 0;
+	NRF_UARTE1->ENABLE = 0;
 }
 void main(void)
 {
@@ -56,20 +55,18 @@ void main(void)
 		blinkCnt++;
 		gpio_pin_set(dev, PIN, (int)led_is_on);
 		led_is_on = !led_is_on;
-		k_msleep(SLEEP_TIME_MS);
-		
-#if 1   /* 1: make sure App core always in idle */		
-		while(1){
-			k_cpu_idle();  // single thread, call this once will be ok. 
-		}
-#endif
 
-#if 0  /* trun off   */
-	if(blinkCnt > 2){
-                //nrf_regulators_dcdcen_set(NRF_REGULATORS,0);
-		nrf_regulators_system_off(NRF_REGULATORS);
-	}
-	
+#if 0 /* 1: make sure App core always in idle */
+		while (1) {
+			k_cpu_idle(); // single thread, call this once will be ok.
+		}
+#else /* trun off   */
+
+		//nrf_regulators_dcdcen_set(NRF_REGULATORS,0);
+		k_msleep(2000);
+		nrf_regulators_system_off(NRF_REGULATORS_NS);
+
 #endif
+		k_msleep(SLEEP_TIME_MS);
 	}
 }
--- app/nrf91/hello_world_lp/src/main.c
+++ app/nrf91/hello_world_lp/src/main.c
@@ -6,22 +6,31 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
-
+#include <modem/lte_lc.h>
 #include <power/power.h>
 #include <hal/nrf_regulators.h>
 
-void disable_uart()  // disable UART to measure current.
+void disable_uart() // disable UART to measure current.
 {
-      NRF_UARTE0->ENABLE = 0;
-      NRF_UARTE1->ENABLE = 0;
+	NRF_UARTE0->ENABLE = 0;
+	NRF_UARTE1->ENABLE = 0;
 }
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	lte_lc_power_off();
+	k_sleep(K_MSEC(1200));
 	disable_uart();
-    nrf_regulators_system_off(NRF_REGULATORS);
-    for (;;) {
-    k_cpu_idle();
-    }
-        
+	k_sleep(K_MSEC(100));
+#if 0 /*   App core off option */
+	nrf_regulators_system_off(NRF_REGULATORS_NS);
+#else /* idle otherwise */
+
+	/* then app core idle   */
+	k_cpu_idle();
+	for (;;) {
+		k_cpu_idle();
+	}
+#endif
 }

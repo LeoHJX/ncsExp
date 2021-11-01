662a30c86216aa58fac9e36c0bfb5abcfcd10886
LeoHJX <hojin421@gmail.com>
2021-11-01 12:54:30 +1100

    add udp echo server

---
+++ python/udp_echo_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = '0.0.0.0'
+server_port = 65003 #linstin this port
+
+server = (server_address, server_port)
+sock.bind(server)
+print("Listening on ", server_address, ":", str(server_port), flush=True)
+
+while True:
+    payload, client_address = sock.recvfrom(4096)
+    print("Echoing back to ", str(client_address), ": ", payload)
+    sent = sock.sendto(payload, client_address)
+    print("Sent len: ", str(sent), flush=True)

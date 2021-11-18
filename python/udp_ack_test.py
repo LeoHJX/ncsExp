e26382358ed536742c3aabb472a6f744cfe069c2
LeoHJX <hojin421@gmail.com>
2021-11-18 22:06:13 +1100

    3 way udp testing server, python. udp server echo test script, python

---
+++ python/udp_ack_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = '0.0.0.0'
+server_port = 65006 #linstin this port
+
+server = (server_address, server_port)
+sock.bind(server)
+print("Listening on ", server_address, ":", str(server_port), flush=True)
+
+while True:
+    payload, client_address = sock.recvfrom(4096)
+    print("packet received: ", str(client_address), "len: ", len(payload), " : ", payload)
+    if(payload.decode("utf-8").find('pkg:') != -1):
+        print("send back to ", str(client_address), ": ", payload[:9])
+        sent = sock.sendto(payload[:9], client_address)
+        print("Sent len: ", str(sent), flush=True)
+
